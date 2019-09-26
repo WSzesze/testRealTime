8a8ca2a7419daebac029f029f5e6c8ba78e1a19d
WSzesze <wong.sze109@gmail.com>
2019-09-26 11:21:58 +0800

    update

--- classifier.py
+++ classifier.py
@@ -4,23 +4,29 @@ from PIL import Image
 import os
 import cv2
 
+data_dir = "C:\\Users\\Sze2\\PycharmProjects\\testRealTime\\pics"
+
+# Method to train custom classifier to recognize face
 def train_classifier(data_dir):
+    #Read all the images in custom data-set
     path = [os.path.join(data_dir, f) for f in os.listdir(data_dir)]
     faces = []
     ids = []
 
+    # Store images in a numpy format and ids of the user on the same indes in imageNp and id L=lists
     for image in path:
         img = Image.open(image).convert('L')
         imageNp = np.array(img, 'uint8')
-        id = int(os.path.split(image)[1].split("."))
+        id = int(os.path.split(image)[1].split(".")[1])
 
         faces.append(imageNp)
         ids.append(id)
 
     ids = np.array(ids)
 
-    clf = cv2.face.LBPHFaceRecognizer_create()
-    clf.train(faces.ids)
+    # Train and save classifier
+    clf = cv2.face_LBPHFaceRecognizer.create()
+    clf.train(faces, ids)
     clf.write("classifier.yml")
 
 train_classifier("data")
